cc0c72de3b98639144ef2d70664c9d5c704b17d2
lecy <jdlecy@gmail.com>
2016-06-08 11:13:18 -0400

    Create panel.cor.R

---
+++ panel.cor.R
@@ -0,0 +1,29 @@
+# add correlation values to a panel plot of bivariate relationships
+
+panel.cor <- function(x, y, digits=2, prefix="", cex.cor)
+{
+    usr <- par("usr"); on.exit(par(usr))
+    par(usr = c(0, 1, 0, 1))
+    r <- abs(cor(x, y))
+    txt <- format(c(r, 0.123456789), digits=digits)[1]
+    txt <- paste(prefix, txt, sep="")
+    if(missing(cex.cor)) cex <- 0.8/strwidth(txt)
+    
+    test <- cor.test(x,y)
+    # borrowed from printCoefmat
+    Signif <- symnum(test$p.value, corr = FALSE, na = FALSE,
+                  cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                  symbols = c("***", "**", "*", ".", " "))
+    
+    text(0.5, 0.5, txt, cex = 1.5 )
+    text(.7, .8, Signif, cex=cex, col=2)
+}
+
+
+# x1 <- rnorm(100)
+# x2 <- 2*x1 + 10*rnorm(100)
+# y <- x1 - 2*x2 + 10*rnorm(100)
+#
+# dat <- data.frame(x1,x2,y)
+#
+# pairs( dat, lower.panel=panel.smooth, upper.panel=panel.cor)
